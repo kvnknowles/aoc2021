5749d9f6ae18066786bdeefb52434e590785245c
Kevin Knowles <knowles.kevin@gmail.com>
2021-12-01 23:10:14 -0600

    Quick and dirty day 2 part 2 - do better

--- 2/solution.py
+++ 2/solution.py
@@ -17,3 +17,20 @@ for x in commands:
         depth -= int(length)
 
 print(depth*horizontal)
+
+depth = 0
+horizontal = 0
+aim = 0
+for x in commands:
+    command, length = x.split(' ')
+    if command.startswith('f'):
+        horizontal += int(length)
+        depth += int(length) * aim
+    if command.startswith('d'):
+        aim += int(length)
+    if command.startswith('u'):
+        aim -= int(length)
+
+print(depth*horizontal)
+
+
